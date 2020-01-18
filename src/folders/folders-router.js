42ec3cde48b214ab805e2432d1a9be50f9d3d285
Grace <mgracebodur@gmail.com>
2020-01-18 02:38:09 -0500

    POST /folders route created, need to fix

--- src/folders/folders-router.js
+++ src/folders/folders-router.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const logger = require('../logger')
 const { folders } = require('../store')
+const uuid = require('uuid/v4')
 
-const jsonParser = express.Router
+const bodyParser = express.Router
 const foldersRouter = express.Router()
 
 foldersRouter
@@ -10,5 +11,21 @@ foldersRouter
     .get((req, res) => {
         res.json(folders)
     })
+    .post(bodyParser, (req, res) => {
+        // const id = uuid()
+        const { name } = req.body
+        const newFolder = { id, name }
 
+        if(!name) {
+            logger.error(`Folder name is required`)
+            return res.status(400).send('Title, url and description are required')
+        }
+
+        folders.push(newFolder)
+
+        logger.info(`Folder with id: ${newFolder.id} created`)
+
+        return res.status(201).location(`http://localhost:8000/folders/${newFolder.id}`).json(newFolder)
+    })
+              
 module.exports = foldersRouter 
