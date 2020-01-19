80fef264e7219977638535b650293fb1516e72cb
Grace <mgracebodur@gmail.com>
2020-01-19 01:24:02 -0500

    Test fixtures created, preparing to test

---
+++ test/fixtures/folders.fixtures.js
@@ -0,0 +1,18 @@
+function makeFoldersArray() {
+    return [
+        {
+            id     : 1,
+            name  : "Orchids",
+        },
+        {
+            id     : 2,
+            name  : "Lilies",
+        },
+        {
+            id     : 3,
+            title  : "Daisies",
+        },
+    ] 
+}
+
+module.exports = { makeFoldersArray }
---
+++ test/fixtures/notes.fixtures.js
@@ -0,0 +1,27 @@
+function makeNotesArray() {
+    return [
+        {
+          "id": "1",
+          "name": "First test post",
+          "modified": "2019-01-03T00:00:00.000Z",
+          "folderId": "1",
+          "content": "Incididunt voluptate sit magna nostrud veniam ea ex mollit enim sint nostrud."
+        },
+        {
+          "id": "2",
+          "name": "Second test post",
+          "modified": "2018-08-15T23:00:00.000Z",
+          "folderId": "2",
+          "content": "Qui mollit in anim pariatur ullamco et proident duis eiusmod consequat."
+        },
+        {
+          "id": "3",
+          "name": "Third test post",
+          "modified": "2018-03-01T00:00:00.000Z",
+          "folderId": "3",
+          "content": "Sunt officia labore dolore laboris tempor enim est et consectetur nostrud est culpa."
+        },
+    ]
+}
+
+module.exports = { makeNotesArray }
