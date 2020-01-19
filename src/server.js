15ccbb5727fbcfe4a6b42e024876f061d8382449
Grace <mgracebodur@gmail.com>
2020-01-19 00:37:23 -0500

    Moved migrations and seed file out of src

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const knex = require('knex')
 const app = require('./app')
 const  { PORT, DB_URL } = require('./config')
@@ -7,6 +6,7 @@ const db = knex({
     client: 'pg',
     connection: DB_URL,
 })
+
 app.set('db', db)
 
 app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`))
